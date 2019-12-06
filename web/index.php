2167ed1df5875e9597041c26a289493cbe6ddf17
casboomdtp@gmail.com <casboomdtp@gmail.com>
2019-12-06 13:57:02 +0100

    demo

--- web/index.php
+++ web/index.php
@@ -1,13 +1,23 @@
 <canvas id='myCanvas' width='400' height='400'>Outdated browser detected!</canvas>
 <script src="main.js"></script>
 <style>
+body{
+  overflow:hidden;
+  margin:0;
+}
 canvas#myCanvas {
   background-color:#000000;
   margin-left:auto;
   margin-right:auto;
   display:inline-block;
 }
+button{
+  width:50%;
+  margin:0;
+  background-color:#0000ff;
+  height:1000px;
+}
 </style>
 <br>
-<button id="right" type="button">Right</button>
-<button id="left" type="button">Left</button>
+<button id="right" type="button"></button>
+<button id="left" type="button"></button>
