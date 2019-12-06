8507a12b0312ee62c008b9c9484f01ecf3c827d8
casboomdtp@gmail.com <casboomdtp@gmail.com>
2019-12-06 13:42:58 +0100

    demo

--- web/index.php
+++ web/index.php
@@ -1,25 +1,13 @@
-<?php
-
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+<canvas id='myCanvas' width='400' height='400'>Outdated browser detected!</canvas>
+<script src="main.js"></script>
+<style>
+canvas#myCanvas {
+  background-color:#000000;
+  margin-left:auto;
+  margin-right:auto;
+  display:inline-block;
+}
+</style>
+<br>
+<button id="right" type="button">Right</button>
+<button id="left" type="button">Left</button>
