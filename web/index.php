c5a60130a9409fa09bc532c2ced5841bae76a9a8
casboomdtp@gmail.com <casboomdtp@gmail.com>
2020-01-10 15:54:31 +0100

    everything git commit -m everything[A

--- web/index.php
+++ web/index.php
@@ -36,6 +36,9 @@ button{
 --><button id="left" type="button"></button>
 </body>
 <script>
-  if(navigator.serviceWorker){
-    navigator.serviceWorker.register('sw.js');
-  }
+ if (!navigator.serviceWorker.controller) {
+     navigator.serviceWorker.register("/sw.js").then(function(reg) {
+         console.log("Service worker has been registered for scope: " + reg.scope);
+     });
+ }
+</script>
